4d65561c3d1891fbcb50b27ca65f3ec0d3c2c37b
Elijah Ren <72220510+elijah15976@users.noreply.github.com>
2024-10-28 03:51:54 +0000

    Proper communication of game creation

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Server-side
+//https://glitch.com/edit/#!/tres-abilities-server
+
 //Create Game Button
 function createGame() {
   document.getElementById("createGame").style = "display: block;";
@@ -39,4 +42,26 @@ function createTheGame() {
  */
 function handleMessage(message) {
   console.log(message);
+
+  //When a user joins a room
+  if(message.includes("Code: ")){
+    let code = message.substring(message.indexOf("Code: ") + 6);
+    let build = `<p>Waiting (${code})...</p>`;
+
+    document.getElementById("joinGame").style = "display: block;";
+    document.getElementById("createGame").style = document.getElementById("joinGame").style;
+
+    document.getElementById("create").setAttribute("disabled", "");
+    document.getElementById("join").setAttribute("disabled", "");
+
+    let preppers = document.getElementsByClassName("prep");
+    for(let i = 0; i < preppers.length; i++) {
+      let element = preppers[i];
+      element.innerHTML = build;
+    }
+  }
+  //Room code cannot be found on server-side
+  else if(message == "Room not found"){
+    alert("Room not found");
+  }
 }
